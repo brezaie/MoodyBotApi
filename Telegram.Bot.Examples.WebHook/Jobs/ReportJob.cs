f8ca2154b28a6586f29af37f192dc32d77ba5452
behzadrezaie11 <behzadrezaie11@gmail.com>
2024-08-29 18:18:35 +0330

    feat: generate report for me

--- Telegram.Bot.Examples.WebHook/Jobs/ReportJob.cs
+++ Telegram.Bot.Examples.WebHook/Jobs/ReportJob.cs
@@ -22,7 +22,7 @@ public class ReportJob : IJob
     public async Task Run()
     {
         var users = await _userRepository.GetAllAsync();
-        foreach (var user in users)
+        foreach (var user in users.Where(x => x.Username == "brezaie"))
         {
             try
             {
